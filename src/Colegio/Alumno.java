ebdef7e3e20d5f81bd2ce39cd8dd48c8f365e2e9
marianalorenav728-byte <marianalorenav728@gmail.com>
2026-09-03 16:41:41 -0300

    en la clase alumno cree atributos,getter y setter,constructor y metodos

--- src/Colegio/Alumno.java
+++ src/Colegio/Alumno.java
@@ -4,10 +4,65 @@
  */
 package Colegio;
 
+import java.util.HashSet;
+
 /**
  *
  * @author ramir
  */
 public class Alumno {
+    private int legajo;
+   private String apellido;
+   private String nombre;
+   private HashSet<Materia> materias;
+
+    public Alumno(int legajo, String apellido, String nombre) {
+        this.legajo = legajo;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.materias = new HashSet<>();;
+    }
+
+
+    public int getLegajo() {
+        return legajo;
+    }
+
+    public void setLegajo(int legajo) {
+        this.legajo = legajo;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public HashSet<Materia> getMaterias() {
+        return materias;
+    }
+    // Método para agregar materia (evita duplicados gracias al HashSet)
+    public boolean agregarMateria(Materia m) {
+        return materias.add(m);
+    }
+
+    // Método para saber la cantidad de materias
+    public int cantidadMaterias() {
+        return materias.size();
+    } 
+
+    @Override
+    public String toString() {
+        return "Alumno{" + "legajo=" + legajo + ", apellido=" + apellido + ", nombre=" + nombre + ", materias=" + materias + '}';
+    }    
     
 }
